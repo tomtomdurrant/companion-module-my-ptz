0f2eae8b0775fc766de19f725482534a3850b360
Tom Durrant <tomchdurrant@gmail.com>
2023-07-18 10:45:35 +0100

    Add some error handling

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,6 @@ class MyInstance extends InstanceBase<DeviceConfig> implements MyInstanceBase {
 		this.config = config
 
 		this.saveConfig(this.config)
-
-		this.initActions()
 	}
 
 	async init(config: DeviceConfig): Promise<void> {
@@ -34,12 +32,13 @@ class MyInstance extends InstanceBase<DeviceConfig> implements MyInstanceBase {
 
 		this.updateStatus(InstanceStatus.Connecting)
 
-		this.initActions()
-		this.initFeedbacks()
-		await this.initVariables().catch((_) => {
-			this.updateStatus(InstanceStatus.ConnectionFailure)
-		})
-		this.updateStatus(InstanceStatus.Ok)
+		await this.initVariables()
+			.catch((err) => {
+				this.log('error', `Error occurred during init - ${JSON.stringify(err)}`)
+				this.updateStatus(InstanceStatus.ConnectionFailure)
+			})
+			.then((_) => this.updateStatus(InstanceStatus.Ok))
+		this.log('info', 'init okay')
 	}
 
 	async destroy(): Promise<void> {
@@ -56,14 +55,6 @@ class MyInstance extends InstanceBase<DeviceConfig> implements MyInstanceBase {
 		return getConfigFields()
 	}
 
-	private initActions() {
-		this.setActionDefinitions({})
-	}
-
-	// Set up Feedbacks
-	private initFeedbacks() {
-		this.setFeedbackDefinitions({})
-	}
 	private parse(currentPreset: string) {
 		const lines = currentPreset.split('\n')
 		let preset = 'Err'
@@ -105,24 +96,28 @@ class MyInstance extends InstanceBase<DeviceConfig> implements MyInstanceBase {
 
 		this.setVariableDefinitions(variables)
 		this.interval = setIntervalAsync(async () => {
-			const [cam1, cam2, cam3, cam4] = await Promise.all([
-				this.checkCamera(1),
-				this.checkCamera(2),
-				this.checkCamera(3),
-				this.checkCamera(4),
-			])
-			// this.setVariableValues({
-			// 	currentPresetCam1: camPresets.currentPresetCam1,
-			// 	currentPresetCam2: camPresets.currentPresetCam2,
-			// 	currentPresetCam3: camPresets.currentPresetCam3,
-			// 	currentPresetCam4: camPresets.currentPresetCam4,
-			// })
+			let cam1
+			let cam2
+			let cam3
+			let cam4
+			try {
+				;[cam1, cam2, cam3, cam4] = await Promise.all([
+					this.checkCamera(1),
+					this.checkCamera(2),
+					this.checkCamera(3),
+					this.checkCamera(4),
+				])
+			} catch (error) {
+				this.log('error', `Failed to retrieve camera information ${JSON.stringify(error)}`)
+				throw error
+			}
 			this.setVariableValues({
 				currentPresetCam1: cam1 as string,
 				currentPresetCam2: cam2 as string,
 				currentPresetCam3: cam3 as string,
 				currentPresetCam4: cam4 as string,
 			})
+			this.log('debug', 'Updated camera values')
 		}, 1000)
 	}
 
@@ -134,12 +129,11 @@ class MyInstance extends InstanceBase<DeviceConfig> implements MyInstanceBase {
 				if (this.config.debug) {
 					this.log('debug', `camera ${camNum} preset is ${preset}`)
 				}
-
-				// this.cameraPresets[`currentPresetCam${camNum}`] = preset
 				return preset
 			})
 			.catch((e) => {
 				this.log('error', JSON.stringify(e))
+				throw e
 			})
 	}
 }
